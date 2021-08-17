807684196247b1cee292d6168cc7f8742a560295
Sanu Ann <sanuann@mit.edu>
2021-08-17 13:29:55 -0400

    refactor and fix directory structure

---
+++ ui-changes/src/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /* eslint-disable */
+  githubSrc: 'https://raw.githubusercontent.com/sanuann/covid19/master/protocol/Covid19_schema',
+  startButton: {
+    "en": 'Join',
+    "es": 'Participar'
+  },
+  assetsPublicPath: '/covid19/',
+  backendServer: 'https://sig.mit.edu/vb',
+  showHelp: true,
+  contact: 'voice_survey@mit.edu',
+  emailSubject: 'Help with Covid19 study',
+  checkMediaPermission: true
+};
+
